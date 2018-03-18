6df7fb2434239d23d74dd458c253c766f072f049
Victor Prokopiev <v.prokopev@corp.mail.ru>
2018-03-18 16:25:16 +0300

    mogile port

--- run.sh
+++ run.sh
@@ -41,28 +41,39 @@ then
   MYSQL_MOGILE_HOST="localhost"
 fi
 
-if [ "`echo ${MYSQL_PORT}`" == "" ]
+if [ "`echo ${MYSQL_MOGILE_PORT}`" == "" ]
 then
-  MYSQL_PORT="3306"
+  MYSQL_MOGILE_PORT="3306"
 fi
 
-mogdbsetup --type=MySQL --yes --dbport=${MYSQL_PORT} --dbhost=${MYSQL_MOGILE_HOST} --dbrootuser=${MYSQL_ROOT_USER} --dbrootpass=${MYSQL_ROOT_PASSWORD} --dbname=${MYSQL_ROOT_PASSWORD} --dbuser=${MYSQL_MOGILE_USER} --dbpassword=${MYSQL_MOGILE_PASSWORD}
+if [ "`echo ${MOGILE_TRACKER_PORT}`" == "" ]
+then
+  MOGILE_TRACKER_PORT="9001"
+fi
+
+if ! mogdbsetup --type=MySQL --yes --dbport=${MYSQL_MOGILE_PORT} --dbhost=${MYSQL_MOGILE_HOST} --dbrootuser=${MYSQL_ROOT_USER} --dbrootpass=${MYSQL_ROOT_PASSWORD} --dbname=${MYSQL_MOGILE_DB} --dbuser=${MYSQL_MOGILE_USER} --dbpassword=${MYSQL_MOGILE_PASSWORD}; then
+    printf '%s\n' 'mogdbsetup: returned error' >&2
+    exit 1
+fi
+
 
 sed -i "s/\MYSQL_MOGILE_HOST/${MYSQL_MOGILE_HOST}/g" /etc/mogilefs/mogilefsd.conf
 sed -i "s/\MYSQL_MOGILE_DB/${MYSQL_MOGILE_DB}/g" /etc/mogilefs/mogilefsd.conf
 sed -i "s/\MYSQL_MOGILE_USER/${MYSQL_MOGILE_USER}/g" /etc/mogilefs/mogilefsd.conf
 sed -i "s/\MYSQL_MOGILE_PASSWORD/${MYSQL_MOGILE_PASSWORD}/g" /etc/mogilefs/mogilefsd.conf
 sed -i "s/\MYSQL_MOGILE_PORT/${MYSQL_MOGILE_PORT}/g" /etc/mogilefs/mogilefsd.conf
+sed -i "s/\MOGILE_TRACKER_PORT/${MOGILE_TRACKER_PORT}/g" /etc/mogilefs/mogilefsd.conf
+sed -i "s/\MOGILE_TRACKER_PORT/${MOGILE_TRACKER_PORT}/g" /root/.mogilefs.conf
 
 sudo -u mogile mogilefsd --daemon -c /etc/mogilefs/mogilefsd.conf
 
-mogadm --trackers=127.0.0.1:7001 host add mogilestorage --ip=${NODE_HOST} --port=${NODE_PORT} --status=alive
-mogadm --trackers=127.0.0.1:7001 device add mogilestorage 1
-mogadm --trackers=127.0.0.1:7001 device add mogilestorage 2
+mogadm --trackers=127.0.0.1:${MOGILE_TRACKER_PORT} host add mogilestorage --ip=${NODE_HOST} --port=${NODE_PORT} --status=alive
+mogadm --trackers=127.0.0.1:${MOGILE_TRACKER_PORT} device add mogilestorage 1
+mogadm --trackers=127.0.0.1:${MOGILE_TRACKER_PORT} device add mogilestorage 2
 
 if [ "`echo ${DOMAIN_NAME}`" != "" ]
 then
-  mogadm --trackers=127.0.0.1:7001 domain add ${DOMAIN_NAME}
+  mogadm --trackers=127.0.0.1:${MOGILE_TRACKER_PORT} domain add ${DOMAIN_NAME}
   mogadm class modify sbf default --replpolicy='MultipleDevices()'
 
   # Add all given classes
@@ -70,7 +81,7 @@ then
   then
     for class in ${CLASS_NAMES}
     do
-      mogadm --trackers=127.0.0.1:7001 class add ${DOMAIN_NAME} $class --replpolicy="MultipleDevices()"
+      mogadm --trackers=127.0.0.1:${MOGILE_TRACKER_PORT} class add ${DOMAIN_NAME} $class --replpolicy="MultipleDevices()"
     done
   fi
 fi
